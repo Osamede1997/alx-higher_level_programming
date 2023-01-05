43a22bd59673b3c362e65c6b539fffaa73bc12f9
Osamede1997 <derrick_rita@hotmail.com>
2023-01-05 07:17:31 -0800

    commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -26,6 +26,6 @@ class Square:
                 raise ValueError("size must be >= 0")
             self.__size = value
 
-        def area(self):
-            """Return the area of the square"""
-            return (self.__size ** 2)
+    def area(self):
+        """Return the area of the square"""
+        return (self.__size ** 2)
