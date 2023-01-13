f529ca205e9752652b25ded7760fa36225e07545
Osamede1997 <derrick_rita@hotmail.com>
2023-01-13 05:00:19 -0800

    commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,10 +5,6 @@
 class Mylist(list):
     """a subclass of list"""
 
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
-
     def print_sorted(self):
         """prints the sorted list"""
         print(sorted(self))
