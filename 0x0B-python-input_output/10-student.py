02207f29eb2aa5c1bc0599d9bb16dfecddb0aa08
Osamede1997 <derrick_rita@hotmail.com>
2023-01-16 05:29:22 -0800

    commit

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a class Student."""
 
+
 class Student:
     """Represent a student."""
     def __init__(self, first_name, last_name, age):
@@ -13,7 +14,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
         If attrs is a list of strings, represents only those attributes
