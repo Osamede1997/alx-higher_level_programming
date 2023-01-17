d7d598ae778f98ac35da2ff15c3fbe598859d412
Osamede1997 <derrick_rita@hotmail.com>
2023-01-17 07:44:49 -0800

    commit

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -49,16 +49,17 @@ class Square(Rectangle):
                 - 4th argument represents y attribute
             **kwargs (dict): New key/value pairs of attributes.
         """
-    if args and len(args) != 0:
+
+        if args and len(args) != 0:
             a = 0
             for arg in args:
                 if a == 0:
                     if arg is None:
-                        self.__init__(self.width, self.x, self.y)
+                        self.__init__(self.size, self.x, self.y)
                     else:
                         self.id = arg
                 elif a == 1:
-                    self.zize = arg
+                    self.size = arg
                 elif a == 2:
                     self.x = arg
                 elif a == 3:
@@ -69,7 +70,7 @@ class Square(Rectangle):
             for k, v in kwargs.items():
                 if k == "id":
                     if v is None:
-                        self.__init__(self.width, self.height, self.x, self.y)
+                        self.__init__(self.size, self.x, self.y)
                     else:
                         self.id = v
                 elif k == "size":
@@ -77,4 +78,4 @@ class Square(Rectangle):
                 elif k == "x":
                     self.x = v
                 elif k == "y":
-                    self.y = v
+                    self.y = v   
