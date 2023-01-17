e76c6afb1ad9a61aaa85cbbb853e9744228fc25c
Osamede1997 <derrick_rita@hotmail.com>
2023-01-17 01:16:14 -0800

    commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,7 +6,7 @@ class Base:
     """Represent the base model.
 
     Represents the "base" for all other classes in project 0x0C*.
-    
+
     Attributes:
     __nb_objects (int): The number of instantiated Bases.
     """
