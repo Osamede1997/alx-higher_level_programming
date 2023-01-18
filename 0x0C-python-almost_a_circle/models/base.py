276a07db48abfea0d126654ce19b1715a708ee2f
Osamede1997 <derrick_rita@hotmail.com>
2023-01-18 05:37:52 -0800

    commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Defines a base class"""
 
+import json
+
 
 class Base:
     """Represent the base model.
@@ -25,3 +27,17 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """function that returns a JSON string representation
+        
+        Args:
+            list_dictionaries (list): A list of dictionaries
+        """
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+
