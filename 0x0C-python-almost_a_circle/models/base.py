7e3c016474bd500b4e0aaa0dcd812d465f75cad8
Osamede1997 <derrick_rita@hotmail.com>
2023-01-18 05:42:27 -0800

    commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,11 +30,10 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """function that returns a JSON string representation
- 
-        Args: list_dictionaries (list): A list of dictionaries
+        """Return the JSON serialization of a list of dicts.
+        Args:
+            list_dictionaries (list): A list of dictionaries.
         """
-
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
