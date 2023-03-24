e65626ff91b7901111419694a4f399e49499c62e
Osamede1997 <derrick_rita@hotmail.com>
2023-03-24 02:36:50 -0700

    Js commit

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
     let len = list.length - 1;
-
-    for (let i = 0; i < len; i++; len--) {
+    let i = 0;
+    while ((len - i) > 0) {
         const tmp = list[len];
         list[len] = list[i];
         list[i] = aux;
+        i++;
+        len--;
     }
     return list;
 }
