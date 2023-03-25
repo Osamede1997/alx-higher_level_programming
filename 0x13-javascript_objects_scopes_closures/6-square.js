0e376888c30d9b09da70cce95ca389cb8755b607
Osamede1997 <derrick_rita@hotmail.com>
2023-03-25 05:00:58 -0700

    Js commit

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,7 +5,7 @@ class Square extends require('./5-square.js') {
   }
 
   charPrint (c) {
-    if (c == undefined) {
+    if (c === undefined) {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
