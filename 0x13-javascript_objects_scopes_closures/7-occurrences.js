d6272fa849499c8ec1f79e603738a9a1c0aa8fff
Osamede1997 <derrick_rita@hotmail.com>
2023-03-25 05:14:50 -0700

    Js commit

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let nO = 0;
-    for (let i = 0; i < list.length; i++) {
-        if (searchElement == list[i]) {
-            nO++;
-        }
-    }
-    return nO;
+  return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
 };
